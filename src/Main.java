a136c60fed4646ac6055d4462c6c0121510f3ac0
POP CODE <lolo4ka69@list.ru>
2022-12-20 20:17:48 +0300

    Mile

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 100; //рублей
         int bonus = 20; //рублей
+        int result = ticket / bonus;
 
         System.out.println("Купили билет за 100 рублей. За каждые 20 рублей, потраченные на билет, начисляется 1 миля");
-        System.out.println((ticket / bonus) + " бонусных миль получено за купленный билет");
+        System.out.println(result + " бонусных миль получено за купленный билет");
 
     }
 }
